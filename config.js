b6c8b0318f83ed96a7c4c41a3d2479fd5c7d4fbc
East Mael <eastmaels@gmail.com>
2021-08-31 23:16:39 +0900

    update config for blurt

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-  node: 'https://api.hive.blog',
-  chain_id: 'beeab0de00000000000000000000000000000000000000000000000000000000',
-  address_prefix: 'STM'
+  node: 'https://rpc.blurt.world',
+  chain_id: 'cd8d90f29ae273abec3eaa7731e25934c63eb654d55080caff2ebb7f5df6381f',
+  address_prefix: 'BLT'
 }
 
 module.exports = config
